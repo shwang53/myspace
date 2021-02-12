b496834137913cdfa061741fe8939ce4c8c9ccf9
Suk Hwang <dev.suuk@gmail.com>
2021-02-12 00:58:26 -0600

    Added read and write using fire storage

--- src/views/storage.vue
+++ src/views/storage.vue
@@ -1,22 +1,32 @@
 <template>
-    <v-sheet>
-        storage
-        <v-btn @click="write">write</v-btn>
-    </v-sheet>
-</template>
+  <v-sheet>
+    <v-textarea v-model="text">
 
+    </v-textarea>
+    <v-btn @click="read">read</v-btn>
+    <v-btn @click="write">write</v-btn>
+  </v-sheet>
+</template>
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
-      text: ''
+      text: '',
+      url: ''
     }
   },
   methods: {
     async write () {
       const sn = await this.$firebase.storage().ref().child('xxx.text').putString('abcd')
       const url = await sn.ref.getDownloadURL()
-      console.log(url)
+      this.url = url
+    },
+    async read () {
+      const r = await axios.get(this.url)
+      console.log(r)
+      this.text = r.data
     }
   }
 }
