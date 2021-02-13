7e6dfb0c3d0fb0a97f388fb4d8a145f80e52e85e
Suk Hwang <dev.suuk@gmail.com>
2021-02-12 23:05:21 -0600

    Added toast ui editor successfully

--- src/plugins/editor.js
+++ src/plugins/editor.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import 'codemirror/lib/codemirror.css'
 import '@toast-ui/editor/dist/toastui-editor.css'
 import '@toast-ui/editor/dist/toastui-editor-viewer.css'
-import { Editor, Viewer } from '@toast-ui/vue-editor'
+import '@toast-ui/editor/dist/i18n/ko-kr'
 
-Vue.component('editorr', Editor)
-Vue.component('toastViewer', Viewer)
+import { Editor, Viewer } from '@toast-ui/vue-editor'
+Vue.component('editor', Editor)
+Vue.component('viewer', Viewer)
