b48b2ec96f3691db1575aa39d08903bb0c160e0c
Suk Hwang <dev.suuk@gmail.com>
2021-02-14 22:14:14 -0600

    Implemented and displayed post-created time

---
+++ src/components/display-time.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-tooltip bottom>
+        <template v-slot:activator="{on}">
+            <span v-on="on">{{ displayTime }}</span>
+        </template>
+        <span>{{time.toLocaleString()}}</span>
+    </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: ['time'],
+  computed: {
+    displayTime () {
+      const createdTime = this.$moment(this.time)
+      const currentTime = this.$moment()
+      const diff = currentTime.diff(createdTime, 'days')
+      if (diff > 0) return createdTime.toDate().toLocaleDateString()
+      return currentTime.fromNow()
+    }
+  }
+
+}
+</script>
